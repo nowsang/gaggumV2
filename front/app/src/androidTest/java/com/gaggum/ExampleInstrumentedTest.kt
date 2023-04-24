5d0ff60793b4f10136d22de16262090a2d04d081
장현혁 <janghh97@gmail.com>
2023-04-24 09:55:20 +0900

    FE_기본코드 수정_230424

--- front/app/src/androidTest/java/com/gaggum/ExampleInstrumentedTest.kt
+++ front/app/src/androidTest/java/com/gaggum/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.hyokiz.front
+package com.gaggum
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hyokiz.front", appContext.packageName)
+        assertEquals("com.gaggum", appContext.packageName)
     }
 }
