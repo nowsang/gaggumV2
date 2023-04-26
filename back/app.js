d378a15ebf717f50c1842716966ef2620fd80480
김경섭 <subsub1221@gmail.com>
2023-04-26 09:24:27 +0900

    BE_turtleAPI추가_230426

--- back/app.js
+++ back/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const turtleRouter = require("./routes/turtle");
+const diaryRouter = require("./routes/diary");
+const plantRouter = require("./routes/plant");
+const userRouter = require("./routes/user");
+const socket  = require("./socket/socket");
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function(req, res){
+app.use('/plant',plantRouter);
+app.use('/turtle',turtleRouter);
+app.use('/diary',diaryRouter);
+app.use('/user',userRouter);
+app.get('/api', function(req, res){
     res.send('hello NodeJs');
 })
 app.listen(8080, () => console.log('8080번 포트 대기'));
+socket.socketStart();
