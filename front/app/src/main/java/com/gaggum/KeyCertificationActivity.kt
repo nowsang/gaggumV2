59335032d73fe93c1edab610806eb7462354376b
장현혁 <janghh97@gmail.com>
2023-04-27 09:55:06 +0900

    FE_터틀봇 키 인증 페이지 구성_230427

---
+++ front/app/src/main/java/com/gaggum/KeyCertificationActivity.kt
@@ -0,0 +1,15 @@
+package com.gaggum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KeyCertificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_key_certification)
+
+    }
+
+}
--- front/app/src/main/java/com/gaggum/SigninActivity.kt
+++ front/app/src/main/java/com/gaggum/SigninActivity.kt
@@ -34,7 +34,7 @@ class SigninActivity : AppCompatActivity() {
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
-                            val intent = Intent(this, MainActivity::class.java)
+                            val intent = Intent(this, KeyCertificationActivity::class.java)
                             startActivity(intent)
                         } else {
                             Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
