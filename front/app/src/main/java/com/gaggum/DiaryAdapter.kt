0f7a49f1bc426173b8fd152307d84d265bcbcda6
이영재 <ap0674@naver.com>
2023-05-02 17:57:43 +0900

    FE_230502_충돌

--- front/app/src/main/java/com/gaggum/DiaryAdapter.kt
+++ front/app/src/main/java/com/gaggum/DiaryAdapter.kt
@@ -1,10 +1,14 @@
 package com.gaggum
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 class DiaryAdapter(private val diaryList: List<diaryInfo>) :
     RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder>() {
@@ -22,13 +26,21 @@ class DiaryAdapter(private val diaryList: List<diaryInfo>) :
         return DiaryViewHolder(itemView)
     }
 
+    @SuppressLint("NewApi")
     override fun onBindViewHolder(holder: DiaryViewHolder, position: Int) {
         val currentItem = diaryList[position]
 
         holder.diaryTitle.text = currentItem.diaryTitle
         holder.diaryMemo.text = currentItem.diaryMemo
-        holder.diaryDate.text = currentItem.diaryDate
+
         holder.diaryImg.text = currentItem.diaryImg
+
+        val dateString = currentItem.diaryDate
+        val formatter = DateTimeFormatter.ISO_DATE_TIME // 린트로 해결
+        val dateTime = LocalDateTime.parse(dateString, formatter)
+        val formattedDate = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        holder.diaryDate.text = formattedDate
+//        holder.diaryDate.text = currentItem.diaryDate
 
     }
 
--- front/app/src/main/java/com/gaggum/DiaryFragment.kt
+++ front/app/src/main/java/com/gaggum/DiaryFragment.kt
@@ -8,18 +8,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.widget.ArrayAdapter
 
 class DiaryFragment : Fragment() {
-
+    private lateinit var diaries: List<diaryInfo>
+//    private val fixedYears = listOf("2020", "2021", "2022", "2023", "2024", "2025")
+//    private val fixedMonths = (1..12).map { String.format("%02d", it) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,7 +30,6 @@ class DiaryFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.singRV)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        // 서버에서 데이터를 가져오는 코드를 별도의 메서드로 분리합니다.
         getDiariesFromServer(view)
 
         return view
@@ -42,14 +42,12 @@ class DiaryFragment : Fragment() {
                 call: Call<GetAllDiariesResponseBody>,
                 response: Response<GetAllDiariesResponseBody>
             ) {
-
                 Log.e("결과", response.toString())
                 if (response.isSuccessful) {
-                    val diaries = response.body()?.data
+                    diaries = response.body()?.data ?: emptyList()
                     Toast.makeText(context, diaries.toString(), Toast.LENGTH_LONG).show()
-                    if (diaries != null) {
-                        val recyclerView = view.findViewById<RecyclerView>(R.id.singRV)
-                        recyclerView.adapter = DiaryAdapter(diaries)
+                    if (diaries.isNotEmpty()) {
+                        setUpDiaryViews(diaries, view)
                     } else {
                         Toast.makeText(
                             requireContext(),
@@ -67,8 +65,97 @@ class DiaryFragment : Fragment() {
                 call: retrofit2.Call<GetAllDiariesResponseBody>,
                 t: Throwable
             ) {
-                Toast.makeText(requireContext(), "다이어리 목록을 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "다이어리 목록을 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT)
+                    .show()
             }
         })
+    }
+
+    private fun setUpDiaryViews(diaries: List<diaryInfo>, view: View) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.singRV)
+        recyclerView.adapter = DiaryAdapter(diaries)
+
+        val testTitles = diaries.map { it.diaryTitle }.distinct()
+        setupSpinner(view, testTitles)
+
+        setupYearMonthSpinners(view) // 인자 diaries를 제거합니다.
+    }
+
+    private fun setupSpinner(view: View, testTitles: List<String>) {
+        val spinner = view.findViewById<Spinner>(R.id.spinner)
+        val allTitles = listOf("전체보기") + testTitles
+        val arrayAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, allTitles)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                spinnerView: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedTitle = allTitles[position]
+                val filteredDiaries = if (selectedTitle == "전체보기") {
+                    diaries
+                } else {
+                    diaries.filter { it.diaryTitle == selectedTitle }
+                }
+                val recyclerView = view.findViewById<RecyclerView>(R.id.singRV)
+                recyclerView.adapter = DiaryAdapter(filteredDiaries)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
+
+    private fun setupYearMonthSpinners(view: View) {
+        val years = listOf("전체", "2020", "2021", "2022", "2023", "2024", "2025")
+        val yearSpinner = view.findViewById<Spinner>(R.id.yearSpinner)
+        val yearAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, years)
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        yearSpinner.adapter = yearAdapter
+
+        val months = listOf("전체", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+        val monthSpinner = view.findViewById<Spinner>(R.id.monthSpinner)
+        val monthAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, months)
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        monthSpinner.adapter = monthAdapter
+
+        yearSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, spinnerView: View?, position: Int, id: Long) {
+                val selectedYear = years[position]
+                val selectedMonth = months[monthSpinner.selectedItemPosition]
+                filterDiariesByYearAndMonth(selectedYear, selectedMonth, diaries, view)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+
+        monthSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, spinnerView: View?, position: Int, id: Long) {
+                val selectedYear = years[yearSpinner.selectedItemPosition]
+                val selectedMonth = months[position]
+                filterDiariesByYearAndMonth(selectedYear, selectedMonth, diaries, view)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
+
+    private fun filterDiariesByYearAndMonth(selectedYear: String, selectedMonth: String, diaries: List<diaryInfo>, view: View?) {
+        val filteredDiaries = diaries.filter {
+            (it.diaryDate.substring(0, 4) == selectedYear || selectedYear == "전체") &&
+                    (it.diaryDate.substring(5, 7) == selectedMonth || selectedMonth == "전체")
+        }
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.singRV)
+        recyclerView?.adapter = DiaryAdapter(filteredDiaries)
     }
 }
