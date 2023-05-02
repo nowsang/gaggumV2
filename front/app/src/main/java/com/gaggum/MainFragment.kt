d206a66ad718ffcd6ac04b2d16f4e641edd42672
장현혁 <janghh97@gmail.com>
2023-05-02 17:55:47 +0900

    FE_main화면_230502

--- front/app/src/main/java/com/gaggum/MainFragment.kt
+++ front/app/src/main/java/com/gaggum/MainFragment.kt
@@ -9,9 +9,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.gaggum.databinding.FragmentMainBinding
 import com.google.firebase.auth.ktx.auth
@@ -19,12 +19,14 @@ import com.google.firebase.ktx.Firebase
 import retrofit2.Call
 import retrofit2.Response
 import java.io.IOException
-import java.util.Locale
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.roundToInt
 
 class MainFragment : Fragment() {
 
     val OPENWEATHER_API_KEY : String = "be002738467412a6651e4278dd3f8c76"
+    val FLOWER_API_KEY : String = "XXL2KwjyRGopX8KOKa1BT7gj3em5fLBeqRHJ3xmUpHboTi9da0bZL9fXntQWh63TkQBodm6xHmgeas8K7yBerg=="
 
     private lateinit var mainActivity: MainActivity
     private lateinit var locationProvider: LocationProvider
@@ -79,7 +81,7 @@ class MainFragment : Fragment() {
         }
 
         fun getWeather(lat: String?, lon: String?) {
-            val service = WeatherObject.service
+            val service = WeatherClient.service
             service
                 .getWeatherData(lat!!, lon!!, OPENWEATHER_API_KEY)
                 .enqueue(object : retrofit2.Callback<WeatherResponseBody> {
@@ -112,17 +114,46 @@ class MainFragment : Fragment() {
 
         updateUI()
         getWeather(lat, lon)
+        getFlower()
+
+        /* ViewPager2 */
+//        binding.mainViewPager.adapter = ViewPagerAdapter(getNeedWaterPlantList())
+        binding.mainViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
 
         return binding.root
     }
+
+    /* ViewPager Items */
+//    private fun getNeedWaterPlantList() : ArrayList<String> {
+//        val service = RetrofitObject.service
+//        service
+//            .getNeedWaterList(1)
+//            .enqueue(object : retrofit2.Callback<NeedWaterResponseBody> {
+//                override fun onResponse(
+//                    call: Call<NeedWaterResponseBody>,
+//                    response: Response<NeedWaterResponseBody>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        val res = response.body()!!
+//                        Log.e("res", res.toString())
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<NeedWaterResponseBody>, t: Throwable) {
+//                    Toast.makeText(mainActivity, "급수 필요한 식물 받아오기 실패", Toast.LENGTH_SHORT).show()
+//                }
+//
+//            })
+//
+//    }
 
     /* 날씨 API */
 
     private fun getCurrentAddress (latitude : Double, longitude : Double) : Address? {
         // 평소에는 getDefault -> 한국은 Korea
         val geoCoder = Geocoder(mainActivity, Locale.KOREA)
-        val addresses :List<Address>
+        val addresses : List<Address>
 
         try {
             addresses = geoCoder.getFromLocation(latitude, longitude, 7) as List<Address>
@@ -141,6 +172,37 @@ class MainFragment : Fragment() {
 
         val address : Address = addresses[0]
         return address
+
+    }
+
+    private fun getFlower() {
+        var cal = Calendar.getInstance()
+        var month = (cal.get(Calendar.MONTH) + 1)
+        var day = cal.get(Calendar.DATE)
+
+        val service = FlowerClient.service
+        service
+            .getFlowerData(FLOWER_API_KEY, month, day)
+            .enqueue(object : retrofit2.Callback<FlowerResponseBody> {
+                override fun onResponse(
+                    call: Call<FlowerResponseBody>,
+                    response: Response<FlowerResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(mainActivity, "꽃 가져오기 성공", Toast.LENGTH_SHORT).show()
+                        Log.e("response", response.toString())
+                    } else {
+                        Log.e("실패", "실패")
+                    }
+                }
+
+                override fun onFailure(call: Call<FlowerResponseBody>, t: Throwable) {
+                    Toast.makeText(mainActivity, "Retrofit 실패", Toast.LENGTH_SHORT).show()
+                    Log.e("call", call.toString())
+                    Log.e("t", t.toString())
+                }
+
+            })
 
     }
 
