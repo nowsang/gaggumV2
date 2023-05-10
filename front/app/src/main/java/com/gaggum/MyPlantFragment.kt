be766af684d65837e63d6532588eac7e27897538
장현혁 <janghh97@gmail.com>
2023-05-10 09:31:04 +0900

    FE_Room DB 완성_230510

--- front/app/src/main/java/com/gaggum/MyPlantFragment.kt
+++ front/app/src/main/java/com/gaggum/MyPlantFragment.kt
@@ -8,7 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.gaggum.databinding.FragmentMyPlantBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
@@ -21,9 +28,16 @@ class MyPlantFragment : Fragment() {
     private lateinit var allPlantsList : ArrayList<allPlants>
     private lateinit var myPlantRVAdapter: MyPlantRVAdapter
 
+    // Room DB
+    private lateinit var db : ClientDatabase
+    private var turtleId : Int = 0
+    private lateinit var user : String
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mainActivity = context as MainActivity
+        db = Room.databaseBuilder(context, ClientDatabase::class.java, "ClientDatabase")
+            .build()
     }
 
     override fun onCreateView(
@@ -32,6 +46,23 @@ class MyPlantFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         val binding = FragmentMyPlantBinding.inflate(inflater, container, false)
+
+        // Room DB
+        user = Firebase.auth.currentUser!!.uid
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val retrievedUser = db.clientDao().getTurtleId(user)
+            if (retrievedUser != null) {
+                withContext(Dispatchers.Main) {
+                    turtleId = retrievedUser.turtleId
+                    getAllPlants()
+
+                }
+            } else {
+                Log.e("실패", "실패하였습니다.")
+            }
+        }
+
         getAllPlants()
 
         val plantRV = binding.myPlantRV
@@ -46,7 +77,7 @@ class MyPlantFragment : Fragment() {
     fun getAllPlants() {
         val service = RetrofitObject.service
         service
-            .getAllPlantsData(1)
+            .getAllPlantsData(turtleId)
             .enqueue(object : Callback<GetAllPlantsResponseBody> {
                 override fun onResponse(
                     call: Call<GetAllPlantsResponseBody>,
