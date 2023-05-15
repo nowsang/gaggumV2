71e3aab874b35a9758462cea50147d95850afa3b
장현혁 <janghh97@gmail.com>
2023-05-15 14:47:40 +0900

    FE_web download 수정_230515

--- web/src/components/download/Download.jsx
+++ web/src/components/download/Download.jsx
@@ -1,13 +1,13 @@
 import "./Download.css"
 
 const Download = () => {
-  const fileURL = "https://drive.google.com/file/d/1Ls0A04spChEz6BHQGAE5yEAmRkgFUYpd/view?usp=share_link"
 
   return (
     <div className="download">
-      <a href={fileURL}>
-        <button className="download__btn">가꿈</button>
-      </a>
+        <button
+          className="download__btn"
+          onClick={() => window.open("https://drive.google.com/file/d/1Ls0A04spChEz6BHQGAE5yEAmRkgFUYpd/view?usp=share_link")}
+        >가꿈</button>
     </div>
   )
 };
