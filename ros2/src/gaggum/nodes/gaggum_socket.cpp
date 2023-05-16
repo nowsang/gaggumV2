df26740500b2e537b9d198540bf07434a6938ecc
DongminYOUN <kkang15794@naver.com>
2023-05-16 13:21:39 +0900

    ROS2_handcontrol test_230516

--- ros2/src/gaggum/nodes/gaggum_socket.cpp
+++ ros2/src/gaggum/nodes/gaggum_socket.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <map>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
@@ -23,24 +24,58 @@ public:
       "MapScan", 10, std::bind(&GaggumSocketNode::map_scan_callback, this, _1));
 
     
-    
     // Connect to the Socket.IO server
     sio_client_.set_open_listener(std::bind(&GaggumSocketNode::on_connected, this));
     // sio_client_.connect("http://localhost:3001");
-    sio_client_.connect("https://k8b101.p.ssafy.io:3001");
+    // sio_client_.connect("https://k8b101.p.ssafy.io:3001");
     
 
     // motor control
     sio_client_.socket()->on("run_motor", [this](sio::event& event) {  
-
+      
       cout << "motor control" << "\n";
 
-      cout << event.get_message()->get_int() << "\n";
+      auto motor_control = event.get_message()->get_map();
+      string mode = motor_control["mode"]->get_string();      
+      
+      
+      // motor 1, 2, 3
+      if (mode == "motor_status") {
+        int motor = motor_control["motor"]->get_int();
+        cout << motor << '\n';
 
-      auto motor_msg = std_msgs::msg::Int32();
-      motor_msg.data = event.get_message()->get_int();
-      move_state->publish(motor_msg);
-    //   move_state->publish(event.get_message()->get_int());
+        auto motor_msg = std_msgs::msg::Int32();
+        motor_msg.data = motor;
+        move_state->publish(motor_msg);
+      }
+
+
+
+    //   cout << event.get_message()->get_int() << "\n";
+
+    //   int motor_data = event.get_message()->get_int();
+
+    //   auto motor_msg = std_msgs::msg::Int32();
+    //   motor_msg.data = motor_data;
+    //   move_state->publish(motor_msg);
+
+    //   sio::message::list socket_msg;
+    //   // plant1, plant2, plant3
+
+    // //   move_state->publish(event.get_message()->get_int());
+    //   // mode = 1 ;
+    //   if (motor_data == -1) {        
+    //     map<string, string> plant = {{"mode", "water_end"}, {"plant", "plant1"}};
+    //     auto obj_msg = sio::object_message::create();
+    //     for (const auto& [key, value] : plant) {
+    //     // value is Int type. -> sio::int_message
+    //     obj_msg->get_map()[key] = sio::string_message::create(value);
+    //     }
+    //     socket_msg.push(obj_msg);        
+
+    //     sio_client_.socket()->emit("run_motor", socket_msg);
+    //   }
+
 
     });
 
@@ -53,7 +88,6 @@ public:
       auto scan_msg = gaggum_msgs::msg::MapScan();
       scan_msg.run = event.get_message()->get_int();
       map_scan_pub->publish(scan_msg);
-
 
 
       cout << event.get_message()->get_int() << "\n";
--- ros2/src/gaggum/nodes/socket_pub.cpp
+++ ros2/src/gaggum/nodes/socket_pub.cpp
@@ -16,8 +16,8 @@ public:
   {
     // Connect to the Socket.IO server
     sio_client_.set_open_listener(std::bind(&SocketIONode::on_connected, this));
-    // sio_client_.connect("http://localhost:3001");
-    sio_client_.connect("https://k8b101.p.ssafy.io:3001");
+    sio_client_.connect("http://localhost:3001");
+    // sio_client_.connect("https://k8b101.p.ssafy.io:3001");
 
     // Create a timer to send messages to the server periodically
     timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&SocketIONode::send_message, this));
@@ -39,24 +39,25 @@ private:
     // msg.push("Hello from ROS 2!");    
     
     // int
-    auto scan_msg = sio::int_message::create(1);
-    msg.push(scan_msg);
+    // auto scan_msg = sio::int_message::create(-1);
+    // msg.push(scan_msg);
 
     // bool
     // auto bool_msg = sio::bool_message::create(false);
     // msg.push(bool_msg);
 
     // map
-    // map<string, int> plant = {{"plant1", 1}, {"plant2", 2}, {"plant3", 3}};
-    // auto obj_msg = sio::object_message::create();
-    // for (const auto& [key, value] : plant) {
-    //     // value is Int type. -> sio::int_message
-    //     obj_msg->get_map()[key] = sio::int_message::create(value);
-    // }    
-    // msg.push(obj_msg);
+    map<string, string> plant = {{"mode", "motor_status"}, {"motor", "2"}};
+    
+    auto obj_msg = sio::object_message::create();
+    for (const auto& [key, value] : plant) {
+        // value is Int type. -> sio::int_message
+        obj_msg->get_map()[key] = sio::string_message::create(value);
+    }    
+    msg.push(obj_msg);
 
 
-    sio_client_.socket()->emit("run_walltracking", msg);
+    sio_client_.socket()->emit("run_motor", msg);
 
     // Log the sent message
     RCLCPP_INFO(this->get_logger(), "Sent message to Socket.IO server: 'Hello from ROS 2!'");
