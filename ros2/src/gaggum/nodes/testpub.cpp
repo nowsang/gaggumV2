6d3e0fb20f10e4b89f400da160dc0eee9560e5cc
DongminYOUN <kkang15794@naver.com>
2023-05-17 15:48:59 +0900

    ROS2_odom Reset Node_230517

--- ros2/src/gaggum/nodes/testpub.cpp
+++ ros2/src/gaggum/nodes/testpub.cpp
@@ -6,8 +6,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "gaggum_msgs/msg/test.hpp"
 #include "gaggum_msgs/msg/detection.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 using namespace std::chrono_literals;
+using namespace std;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
@@ -18,7 +20,7 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("minimal_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<gaggum_msgs::msg::Test>("topic", 10);
+      publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
       pub_ = this->create_publisher<gaggum_msgs::msg::Detection>("yolo", 100);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
@@ -27,19 +29,24 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = gaggum_msgs::msg::Test();
-      auto my_msg = gaggum_msgs::msg::Detection();
+      cout << "timer" << '\n';
+      // auto message = gaggum_msgs::msg::Test();
+      // auto my_msg = gaggum_msgs::msg::Detection();
+      auto odom_msg = nav_msgs::msg::Odometry();
+      odom_msg.pose.pose.position.x = 0.0;
+      odom_msg.pose.pose.position.y = 0.0;
+      odom_msg.pose.pose.position.z = 0.0;
 
-      message.num = this -> count_++;
-      my_msg.num_index = 12;
+      // message.num = this -> count_++;
+      // my_msg.num_index = 12;
 
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.num);
-      publisher_->publish(message);
-      pub_->publish(my_msg);
+      // RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.num);
+      publisher_->publish(odom_msg);
+      // pub_->publish(my_msg);
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<gaggum_msgs::msg::Test>::SharedPtr publisher_; 
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_; 
     rclcpp::Publisher<gaggum_msgs::msg::Detection>::SharedPtr pub_; 
     size_t count_;
 };
---
+++ ros2/src/gaggum_python/gaggum_python/odom_reset.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+
+
+class odomReset(Node):
+    def __init__(self):
+        super().__init__('odom_reset')
+    # subscriber-----------------------------------
+        self.odom_sub = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+
+    
+    # publisher------------------------------------
+        self.odom_pub = self.create_publisher(Odometry, '/odom', 10)
+
+    # # timer_callback
+    #     timer_period = 0.5  # seconds
+    #     self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+    # callback 상태 확인용 ---------------------------
+        self.is_odom = True        # Odometry
+
+
+    # 메시지-----------------------------------------
+        # self.odom_msg = Odometry()      # odom
+
+
+
+    def odom_callback(self, msg):
+        if self.is_odom:
+            print("callback start!!!!!!!")
+            msg.pose.pose.position.x = 0.0
+            msg.pose.pose.position.y = 0.0
+            msg.pose.pose.position.z = 0.0
+            self.is_odom = False
+            self.odom_pub.publish(msg)
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    odom_reset = odomReset()
+
+    rclpy.spin(odom_reset)
+
+    odom_reset.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
