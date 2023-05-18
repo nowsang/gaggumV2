9761332f88a99332bab223f64bcea7368c5c21a6
장현혁 <janghh97@gmail.com>
2023-05-18 10:21:21 +0900

    FE_web 사이트 변경_230518

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import logo from "./assets/logo/logo.png"
 import Download from './components/download/Download';
+import bgImg from "./assets/app/plants_background.gif"
 
 function App() {
 
@@ -14,6 +15,11 @@ function App() {
       />
       <h1 className='gaggum__title'>당신의 식물을 책임져드립니다.</h1>
       <Download />
+      <img 
+        className="gaggum__bakcground"
+        src={bgImg}
+        alt=""
+      />
     </div>
   );
 }
--- web/src/components/download/Download.jsx
+++ web/src/components/download/Download.jsx
@@ -7,7 +7,7 @@ const Download = () => {
         <button
           className="download__btn"
           onClick={() => window.open("https://drive.google.com/file/d/1Ls0A04spChEz6BHQGAE5yEAmRkgFUYpd/view?usp=share_link")}
-        >가꿈</button>
+        >Download</button>
     </div>
   )
 };
