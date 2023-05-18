d6235b77d6c6c66a2d45c013670cadde36f57063
이준호 <wnsgh0672@naver.com>
2023-05-18 17:59:05 +0900

    ROS2_아 미세한 수치만 조정하면 되는데_230518

--- ros2/src/gaggum_python/gaggum_python/navigation.py
+++ ros2/src/gaggum_python/gaggum_python/navigation.py
@@ -6,6 +6,8 @@ from gaggum_msgs.msg import Detection
 from nav_msgs.msg import Odometry, Path
 from std_msgs.msg import Int16, Int32
 import time
+from rclpy.duration import Duration
+# from nav2_simple_commander.robot_navigator import BasicNavigator
 
 class Navigation(Node):
     def __init__(self):
@@ -18,10 +20,9 @@ class Navigation(Node):
         self.sonar_sub = self.create_subscription(Int32, 'sonar', self.sonar_callback, 10)
 
     # publisher------------------------------------
-        self.goal_pose_pub = self.create_publisher(PoseStamped, '/goal_pose', 10)
-        self.twist_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.water_pub = self.create_publisher(Int16, '/water', 10)
-        self.move_sun_pub = self.create_publisher(Int16, '/move_sun', 10)
+        self.goal_pose_pub = self.create_publisher(PoseStamped, '/goal_pose', 100)
+        self.twist_pub = self.create_publisher(Twist, '/cmd_vel', 100)
+        self.motor_pub = self.create_publisher(Int32, '/motor', 100)
 
     # timer_callback
         timer_period = 0.5  # seconds
@@ -36,15 +37,19 @@ class Navigation(Node):
         self.is_plant_detected = False  # 화분 감지 확인
         self.is_center = False          # 중앙 정렬
         self.is_sonar = False           # 초음파 센서
+        self.is_approch = False         # 화분에 근접
+        self.is_working = True
+        
 
     # 메시지------------------------------------------
         self.pose_msg = PoseStamped()   # nav2에 목표 좌표 보냄
         self.odom_msg = Odometry()      # odom
         self.plan_msg = Path()          # 터틀봇의 이동 경로 좌표
         self.twist_msg = Twist()        # 터틀봇 움직임 제어
-        self.water_msg = Int16()        # 물 주라는 메시지
-        self.move_sun_msg = Int16()     # 햇빛 이동
+        # self.water_msg = Int16()        # 물 주라는 메시지
+        # self.move_sun_msg = Int16()     # 햇빛 이동
         self.yolo_msg = Detection()
+        self.motor_msg = Int32()
 
     # 데이터------------------------------------------
         # 백에서 넘어오는 데이터 더미
@@ -80,51 +85,37 @@ class Navigation(Node):
         self.yolo_cy = 0            # 화분의 중심 y좌표
         self.yolo_view_width = 640  # 카메라 이미지의 넓이
 
-        # centerposion 관련
-        self.tolerance = 3          # 중심 맞추기 오차 허용치
-        self.check_center = 0       # 중심이 맞춰진 횟수
+
+        # plan
+        self.pose_msg.pose.position.x = 1.5
+        self.pose_msg.pose.position.y = 0.16
+        self.pose_msg.pose.orientation.z = 0.0
+        self.pose_msg.header.frame_id = "map"
+
+        self.count  = 0
+        self.water_cnt = 0
 
     def timer_callback(self):
         print(self.is_odom, self.is_yolo, self.is_plan, self.is_path_tracking, self.is_twist)
         if self.is_odom and self.is_yolo and self.is_twist: # 시작 조건 odom, yolo, twist의 값이 넘어올 때
-        
+            print('asdasdasd', self.is_path_tracking, self.is_plan)
             if self.is_path_tracking:    # 터틀봇이 길따라 가는 중
 
                 if self.is_plan:    # 목표 좌표가 넘어오면
-                    self.pose_msg.header.frame_id = "map"
-                    # 터틀봇의 목표 위치
-                    # self.pose_msg.pose.position.x = self.triggers['data']['plant_position_x']
-                    # self.pose_msg.pose.position.y = self.triggers['data']['plant_position_y']
-                    self.pose_msg.pose.position.x = 1.5
-                    self.pose_msg.pose.position.y = 0.16
-                    # self.pose_msg.pose.position.x = -0.7
-                    # self.pose_msg.pose.position.y = 1.9
-
-                    # 터틀봇의 방향
-                    self.pose_msg.pose.orientation.x = 0.0
-                    self.pose_msg.pose.orientation.y = 0.0
-                    self.pose_msg.pose.orientation.w = 1.0
+                    
                     position = self.plan_msg.poses[0].pose.position
                     # 대략 0.2 정도의 오차가 발생함
                     # print(position.x, position.y)
                     x = self.pose_msg.pose.position.x
                     y = self.pose_msg.pose.position.y
-                    
+                
                     # 목표 지점 1 범위 이내에 들어왔으면
                     print("목표", x, y)
                     print("현재", position.x, position.y)
-                    if x - 0.3 < position.x < x + 0.3 and y - 0.3 < position.y <  y + 0.3:
+                    if x - 0.4 < position.x < x + 0.4 and y - 0.4 < position.y <  y + 0.4:
                         print("목표 지점 도착")
-                        self.is_plan = False
                         self.is_path_tracking = False
-                    else:            
-                        # 화분이 50cm 거리 이내에 감지되면
-                        if self.yolo_distance <= 400:
-                            print("화분 감지")
-                            # if  self.yolo_number == self.plant_number - 1:  # 목표 화분인지 확인
-                            self.is_plan = False
-                            self.is_path_tracking = False
-                            self.is_plant_detected = True 
+                        self.is_plan = False
 
                 self.goal_pose_pub.publish(self.pose_msg)               
                 
@@ -132,43 +123,67 @@ class Navigation(Node):
                 print('Publishing: "%s"' % self.pose_msg.pose.position)
 
             else:
-                # 터틀봇을 멈춘 상태
-                self.twist_msg.linear.x = 0.0
-                self.twist_msg.angular.z = 0.0
-                print('터틀봇 멈춤')
-                # 
-                if self.yolo_distance <= 400: 
-                    print("화분 감지")
-                    # 식물이 감지 되었으면
-                    if self.yolo_distance > 240:    # 화분과의 거리가 230이 될 때까지 전진 
-                        self.twist_msg.linear.x = 0.01
+                print(self.is_working)
+                if self.is_working:
                     print("화분과의 거리:",self.yolo_distance)
-                    if self.is_center:                      # 중심 맞춰지면
-                        self.twist_msg.linear.x = 0.05      # 전진 
-                        print(self.sonar_distance)
-                        if self.sonar_distance <= 27:       # 근접하면
-                            print('근접 완료')
-                            # 초음파 센서의 근접의 한계가 있기 때문에 의도적으로 조금만 더 앞으로 보냄
-                            self.twist_msg.linear.x = 0.1
+                    # 화분에 접근하는 거
+                    print("화분 감지")
+                    if self.yolo_distance <= 400: 
+                        if self.is_center:                      # 중심 맞춰지면
+                            self.twist_msg.linear.x = 0.05      # 전진 
+                            self.twist_msg.angular.z = 0.0 
+
+                            print(self.sonar_distance)
+                            if self.sonar_distance <= 27:  # 근접하면
+                                print('근접 완료')
+                                self.twist_msg.linear.x = 0.02
+                                self.twist_pub.publish(self.twist_msg)
+                                time.sleep(3.0)
+                                self.twist_msg.linear.x = 0.0
+                                self.is_approch = True
+                            elif self.sonar_distance <= 25:
+                                self.twist_msg.linear.x = - 0.1
+                        else:                                  
+                            self.centerPositioning()            # 중심 맞추기
+                    else:                                       # 식물이 감지 되지 않음
+                        self.twist_msg.angular.z = 0.2          # 제자리 회전
+                    
+                    # 접근 했을 때
+                    if self.is_approch:
+                        if self.plant_datas['mode'] == 100 and self.water_cnt < 1:
+                            print('물 주기')
+                            self.motor_msg.data = 1
+                            self.motor_pub.publish(self.motor_msg)
+                            print('물 주는 중')
+                            time.sleep(3.0)
+                            self.water_cnt += 1
                             
-                            self.twist_msg.linear.x = 0.0   # 멈춤
+                            print('화분 드는 중')
+                            self.motor_msg.data = 2
+                            self.motor_pub.publish(self.motor_msg)
+                            time.sleep(7.0)
 
-                    else:                                  
-                        self.centerPositioning()            # 중심 맞추기
-                else:                               # 식물이 감지 되지 않음
-                    self.twist_msg.angular.z = 0.2  # 제자리 회전
-                
-                if self.sonar_distance <= 27:
-                    if self.plant_datas['mode'] == 100:
-                        print('물 주기')
-                        # 물 주기 노드에 publish
-                        self.water_msg.data = 1
-                        self.water_pub.publish(self.water_msg)
+                            self.moveSunPoint()
 
-                    elif self.plant_datas['mode'] == 200:
-                        print('햇빛 이동')
-                        self.move_sun_msg.data = 1
-                        self.move_sun_pub.publish(self.move_sun_msg)
+                            self.is_path_tracking = True
+                            self.is_plan = True
+                            
+                        else:
+                            print('화분 내리는 중')
+                            time.sleep(5.0)
+                            self.motor_msg.data = 3
+                            self.motor_pub.publish(self.motor_msg)
+                            time.sleep(10.0)
+
+                            self.moveStartPoint()
+                            self.is_working = False
+
+
+                else:
+                    self.twist_msg.linear.x = -0.1
+                    if self.yolo_distance > 250:
+                        self.twist_msg.linear.x = 0.0
+                        self.is_path_tracking = True
         
         self.twist_pub.publish(self.twist_msg)
             
@@ -180,6 +195,7 @@ class Navigation(Node):
         self.odom_msg = msg
     
     def plan_callback(self, msg):
+        print('aasdfasdfasdasd')
         self.is_plan = True
         self.plan_msg = msg # 이동 좌표가에 담겨있음
 
@@ -209,19 +225,40 @@ class Navigation(Node):
     # 커스텀 함수 ---------------------------
     def centerPositioning(self):
         print('중심을 맞추자') 
-        min_x = self.yolo_view_width / 2 - self.tolerance
-        max_x = self.yolo_view_width / 2 + self.tolerance
+        center = 342
+        min_x = center - 2
+        max_x = center + 2
         
         print(min_x,'<=', self.yolo_cx, '<=', max_x)
         if min_x <= self.yolo_cx <= max_x:        # 중심 좌표가 허용치 내에 있으면
-            self.is_center = True
-            print('중앙 정렬 완료')
-            return
+            self.count += 1
+            print('중앙 정렬', self.count)
+            if self.count > 5:
+                self.is_center = True
+                print('중앙 정렬 완료')
+                return
         else:                                     # 중심이 안 맞으면
             if self.yolo_cx < min_x:              # 중심이 왼쪽에 있으면  
-                self.twist_msg.angular.z = 0.1    # 왼쪽으로 회전           
+                self.twist_msg.angular.z = 0.1    # 왼쪽으로 회전    
+                if self.yolo_cx < min_x - 60:
+                    self.twist_msg.angular.z = 0.3    # 왼쪽으로 회전 
+
             elif self.yolo_cx > max_x:            # 중심이 오른쪽에 있으면
                 self.twist_msg.angular.z = - 0.1  # 오른쪽으로 회전
+                if self.yolo_cx > min_x + 60:
+                    self.twist_msg.angular.z = - 0.3    # 왼쪽으로 회전 
+
+    def moveStartPoint(self):
+        self.pose_msg.pose.position.x = 0.0
+        self.pose_msg.pose.position.y = 0.0
+        self.pose_msg.pose.orientation.z = 0.0
+    
+    def moveSunPoint(self):
+        self.pose_msg.pose.position.x = 1.0
+        self.pose_msg.pose.position.y = 1.27
+        self.pose_msg.pose.orientation.z = 1.0
+
+
 
 def main(args=None):
     rclpy.init(args=args)
